c47be180e746009009f82fe5e86e96884cbd37aa
johan <johan@LAPTOP-A84QGLAV>
2018-02-23 20:52:51 +0100

    refactoring in progress

--- src/main/java/se/johan/oh/dataaccess/UsersDataAccess.java
+++ src/main/java/se/johan/oh/dataaccess/UsersDataAccess.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package se.johan.oh.dataaccess;
 
 
@@ -20,9 +16,8 @@ import java.util.List;
  */
 public class UsersDataAccess {
     
-     ConnectionHandlerInterface connectionHandler;
-
-    UsersDataAccess(ConnectionHandlerInterface connectionHandler) {
+    ConnectionHandlerInterface connectionHandler;
+    UsersDataAccess(ConnectionHandlerInterface connectionHandler){
         this.connectionHandler = connectionHandler;
     }
     
