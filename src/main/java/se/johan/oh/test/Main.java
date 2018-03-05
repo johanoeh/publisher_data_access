134eee647c1ff11fe9cc59842a62d1f97d151374
johan <johan@LAPTOP-A84QGLAV>
2018-03-05 12:34:31 +0100

    refactoring in progress

--- src/main/java/se/johan/oh/test/Main.java
+++ src/main/java/se/johan/oh/test/Main.java
@@ -43,29 +43,6 @@ public class Main {
         SaxParserHandler parserHandler = new SaxParserHandler(resource);
         parserHandler.parseXML();
         
-        //ConnectionHandler connectionHandler = new ConnectionHandler("jdbc:derby://localhost:1527/admin;create=true;user=admin;password=1234");
-        
-
-        /*
-        Connection connection = connectionHandler.getConnection();
-        if(connection == null)
-        System.err.println("Connection is null");
-        else
-        System.out.println("Connection is not null");
-        Statement statement = connection.createStatement();
-        statement.execute(Tables.SUBJECT_TABLE);
-        /*ConnectionHandler connectionManager = new ConnectionHandler(
-        "org.apache.derby.jdbc.ClientDriver",
-        "jdbc:derby://localhost:1527/test;create=true",
-        "admin",
-        "1234");
-        Connection connection = connectionManager.getConnection();*/
-        /*FileReaderHandler fileReaderHandler = new FileReaderHandler(resource);
-        BufferedReader bufferedReader = fileReaderHandler.getBufferedReader("file/publish_SQL.sql");
-        SQLScriptParser  sQLScriptParser = new SQLScriptParser(bufferedReader, new SQLExecutableIMPL(connectionHandler));
-        sQLScriptParser.parse();*/
-        
-        
     }
     
 }
