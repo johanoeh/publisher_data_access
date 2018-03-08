601e48f2e61d5c3fa60907bdf95efe4b5aa7d292
johan <johan@LAPTOP-A84QGLAV>
2018-03-08 19:32:29 +0100

    refactoring in progress

--- src/main/java/se/johan/oh/test/Main.java
+++ src/main/java/se/johan/oh/test/Main.java
@@ -6,19 +6,10 @@
 package se.johan.oh.test;
 
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.sql.Connection;
 import java.sql.SQLException;
-import java.sql.Statement;
 import javax.xml.parsers.ParserConfigurationException;
 import org.xml.sax.SAXException;
-import se.johan.oh.SQL.SQLExecutableIMPL;
-import se.johan.oh.SQL.SQLScriptParser;
-import se.johan.oh.connection.ConnectionHandler;
-
-import se.johan.oh.dataaccess.Tables;
-import se.johan.oh.utils.FileReaderHandler;
 import se.johan.oh.xml.SaxParserHandler;
 
 
@@ -28,9 +19,10 @@ import se.johan.oh.xml.SaxParserHandler;
  */
 public class Main {
 
-    private static final  String URL ="jdbc:derby://localhost:1527/";
-    private static final  String USER_NAME ="admin";
-    private static final  String PASSWORD="1234";
+    private static final  String URL = "jdbc:derby://localhost:1527/";
+    private static final  String USER_NAME = "admin";
+    private static final  String PASSWORD = "1234";
+    
     /**
      * @param args the command line arguments
      * @throws org.xml.sax.SAXException
@@ -38,11 +30,8 @@ public class Main {
      * @throws java.io.IOException
      */
     public static void main(String[] args) throws SAXException, ParserConfigurationException, IOException, SQLException {
-        
         String resource ="file/test.xml";
         SaxParserHandler parserHandler = new SaxParserHandler(resource);
-        parserHandler.parseXML();
-        
-    }
-    
+        parserHandler.parseXML();    
+    }    
 }
